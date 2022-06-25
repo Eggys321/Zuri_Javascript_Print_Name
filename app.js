95067cc393196b91d10d044706cf5ed665f42482
Eggys321 <imonieroheguono@gmail.com>
2022-06-25 14:02:29 +0100

    modified app.js

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ let heightInFoot = 5.7;
 console.log(heightInFoot);
 let country = 'Nigeria';
 console.log(country);
+
+document.write('Eguono Imonieroh');
+document.write(5.7);
+document.write('Nigeria')
